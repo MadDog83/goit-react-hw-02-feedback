3a1416aab8c32e558841c98ec3630312eff4708c
MadDog83 <dimas1083@gmail.com>
2024-10-20 19:37:33 +0200

    add chat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Feedback from './Feedback/Feedback';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
+import Chat from './Chat/Chat';
 
 export class App extends Component {
   state = {
@@ -20,34 +21,40 @@ export class App extends Component {
   countTotalFeedback = () => this.state.good + this.state.neutral + this.state.bad;
 
   countPositiveFeedbackPercentage = () => (this.state.good / this.countTotalFeedback()) * 100;
+
   render() {
     const centerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh'
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh'
     };
 
     return (
-        <div style={centerStyle}>
-            <Section title="Please leave feedback">
-                <Feedback options={['good', 'neutral', 'bad']} onLeaveFeedback={this.handleFeedback} />
-            </Section>
-            <Section title="Statistics">
-                {this.countTotalFeedback() > 0 ? (
-                    <Statistics
-                        good={this.state.good}
-                        neutral={this.state.neutral}
-                        bad={this.state.bad}
-                        total={this.countTotalFeedback()}
-                        positivePercentage={this.countPositiveFeedbackPercentage()}
-                    />
-                ) : (
-                    <Notification message="There is no feedback" />
-                )}
-            </Section>
-        </div>
+      <div style={centerStyle}>
+        <Section title="Please leave feedback">
+          <Feedback options={['good', 'neutral', 'bad']} onLeaveFeedback={this.handleFeedback} />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
+        <Section title="Chat">
+          <Chat />
+        </Section>
+      </div>
     );
-  } 
+  }
 }
+
+export default App;
